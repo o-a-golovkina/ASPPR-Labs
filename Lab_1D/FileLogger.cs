928a3a9b54f29a26c8fa5d31eed4692d83aab53c
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2026-03-31 01:57:28 +0300

    Create an interface ILogger and a class FileLogger that use the new interface

---
+++ Lab_1D/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿public class FileLogger : ILogger
+{
+    private readonly string _filePath;
+
+    public FileLogger(string filePath)
+    {
+        _filePath = filePath;
+        File.WriteAllText(_filePath, $"--- Протокол запуску: {DateTime.Now} ---\n\n");
+    }
+
+    public void Log(string message)
+    {
+        File.AppendAllText(_filePath, message + "\n");
+    }
+}
+}

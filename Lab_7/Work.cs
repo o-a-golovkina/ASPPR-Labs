df598c227e4e0a9b57b4e9555e0b0d815f859dcf
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2026-05-14 12:15:20 +0300

    Fix bugs in class Work

--- Lab_7/Work.cs
+++ Lab_7/Work.cs
@@ -2,25 +2,23 @@
 {
     public class Work
     {
-        class Work
+        public int Id { get; set; }
+        public List<int> Previous { get; set; }
+        public int Duration { get; set; }
+        public int People { get; set; }
+
+        public int EarlyStart { get; set; }
+        public int EarlyFinish { get; set; }
+        public int LateStart { get; set; }
+        public int LateFinish { get; set; }
+        public int Reserve { get; set; }
+
+        public Work(int id, List<int> previous, int duration, int people)
         {
-            public int Id { get; set; }
-            public List<int> Previous { get; set; }
-            public int Duration { get; set; }
-            public int People { get; set; }
-
-            public int EarlyStart { get; set; }
-            public int EarlyFinish { get; set; }
-            public int LateStart { get; set; }
-            public int LateFinish { get; set; }
-            public int Reserve { get; set; }
-
-            public Work(int id, List<int> previous, int duration, int people)
-            {
-                Id = id;
-                Previous = previous;
-                Duration = duration;
-                People = people;
-            }
+            Id = id;
+            Previous = previous;
+            Duration = duration;
+            People = people;
         }
     }
+}
